d78b17e765418e08c5bbfb9230f1e13b0c411371
ytx <txiaopk@gmail.com>
2021-12-24 14:52:52 -0500

    Added resume pdf

--- src/App.js
+++ src/App.js
@@ -10,49 +10,27 @@ export default function App() {
     <>
       <Router>
         <main>
-          <Nav></Nav>
+          <Nav />
           <div className="box-container">
             <Switch>
               <Route exact path="/"><Home /></Route>
               <Route exact path="/resume"><Resume /></Route>
+              <Route exact path="/projects"><Resume /></Route>
             </Switch>
           </div>
-          <Footer></Footer>
+          <Footer />
         </main>
       </Router>
     </>
   );
 }
 
-function Home() {
-  return (
-    <>
-      <Header />
-      <About />
-      <Contact />
-    </>
-  );
-}
-
-function Resume() {
-  return (
-    <>
-      <Contact />
-    </>
-  );
-}
-
-function Button(props) {
-  return (
-    <Link to={props.to} className="button">{props.text}</Link>
-  );
-}
-
+// general components
 function Nav() {
   return (
     <nav>
       <div className="nav-name-container">
-        <Logo></Logo>
+        <Logo />
         <Link to="/#" className="link">Yu Xiao</Link>
       </div>
       <div className="nav-links-container">
@@ -61,8 +39,8 @@ function Nav() {
         <Link to="/resume" className="link">Resume</Link>
         <Link to="/projects" className="link">Projects</Link>
       </div>
-      <Social></Social>
-      <Button to="/#contact" text="Contact"></Button>
+      <Social />
+      <Button to="/#contact" text="Contact" />
     </nav>
   );
 }
@@ -71,7 +49,7 @@ function Logo() {
   return (
     <div>
       <Link to="/#">
-        <img src="./images/sig.png" 
+        <img src="./assets/sig.png" 
             alt="Yu Xiao signature" 
             height="20" 
             className="logo"/>
@@ -93,72 +71,6 @@ function Social() {
          href="https://www.instagram.com/tx_pk/" 
          target="_blank"></a>
     </div>
-  );
-}
-
-function Header() {
-  return (
-    <header>
-      <div className="intro-container">
-        <h1>Hi, I'm Yu Xiao.</h1>
-        <p>
-          A curious programmer and artist 
-          with a heart to better the world 
-          through technology.
-        </p>
-        <Social></Social>
-        <Button to="/#contact" text="Contact"></Button>
-      </div>
-      <figure>
-        <img src="./images/header-1.png" 
-             alt="Samurai illustration"
-             width="450"/>
-      </figure>
-    </header>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <span className="anchor" id="about"></span>
-      <section className="about">
-        <div className="about-container">
-          <h1>About Me</h1>
-          <p>Curious and self learner with
-            a hope to make the world a better
-            place. I have experience in
-            programming, front-end web development,
-            and graphic design through my time at
-            the University of Michigan studying 
-            computer science and independent
-            projects. I come from Greater Boston
-            and am open to relocation! Check out 
-            my experience and projects below:
-          </p>
-          <Button to="/resume" text="Resume"></Button>
-          <Button to="/projects" text="Projects"></Button>
-        </div>
-      </section>
-    </>
-  );
-}
-
-function Contact() {
-  return (
-    <>
-      <span className="anchor" id="contact"></span>
-      <section className="contact">
-        <div className="contact-container">
-          <h1>Contact</h1>
-          {/* <p>(Click below to copy)</p> */}
-          <p>txiaopk@gmail.com</p>
-          <p>978-888-8886</p>
-          <Social></Social>
-          <Button to="/#" text="Back to Top"></Button>
-        </div>
-      </section>
-    </>
   );
 }
 
@@ -197,5 +109,97 @@ function Footer() {
         <p className="copyright">© Yu Xiao 2021. All rights reserved</p>
       </div>
     </footer>
+  );
+}
+
+// home page components
+function Home() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <div className="intro-container">
+        <h1>Hi, I'm Yu Xiao.</h1>
+        <p>
+          A curious programmer and artist 
+          with a heart to better the world 
+          through technology.
+        </p>
+        <Social />
+        <Button to="/#contact" text="Contact" />
+      </div>
+      <figure>
+        <img src="./assets/header-1.png" 
+             alt="Samurai illustration"
+             width="450"/>
+      </figure>
+    </header>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <span className="anchor" id="about"></span>
+      <section className="about">
+        <div className="about-container">
+          <h1>About Me</h1>
+          <p>Curious and self learner with
+            a hope to make the world a better
+            place. I have experience in
+            programming, front-end web development,
+            and graphic design through my time at
+            the University of Michigan studying 
+            computer science and independent
+            projects. I come from Greater Boston
+            and am open to relocation! Check out 
+            my experience and projects below:
+          </p>
+          <Button to="/resume" text="Resume" />
+          <Button to="/projects" text="Projects" />
+        </div>
+      </section>
+    </>
+  );
+}
+
+function Contact() {
+  return (
+    <>
+      <span className="anchor" id="contact"></span>
+      <section className="contact">
+        <div className="contact-container">
+          <h1>Contact</h1>
+          {/* <p>(Click below to copy)</p> */}
+          <p>txiaopk@gmail.com</p>
+          <p>978-888-8886</p>
+          <Social />
+          <Button to="/#" text="Back to Top" />
+        </div>
+      </section>
+    </>
+  );
+}
+
+// resume page components
+function Resume() {
+  return (
+    <>
+      <Contact />
+    </>
+  );
+}
+
+function Button(props) {
+  return (
+    <Link to={props.to} className="button">{props.text}</Link>
   );
 }
