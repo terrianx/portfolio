1ca33b8c04a66ee0cd4b3c7be47b085662f54ae4
ytx <txiaopk@gmail.com>
2021-12-25 19:47:36 -0500

    Added BlueAir project descriptions and links

--- src/App.js
+++ src/App.js
@@ -241,14 +241,44 @@ function Projects() {
                src="./assets/FreshTea.png"
                width="600px"
                alt="FreshTea Website home page"
-               site="https://yu-xiao-boba-shop.netlify.app/index.html"
+               site="https://yu-xiao-boba-shop.netlify.app/"
                code="https://github.com/ytxpk/boba-website"
                tech=" HTML, CSS, JS">
-        <p>Hihihihihi hihihihihihihi</p>
+        <p>I created this mock boba tea website to 
+           gain working knowledge of front end web 
+           development. Additionally, I am a fan of
+           bubble tea and learned more about the history
+           of the drink. I began by creating the site with
+           HTML and CSS. Later, I learned and added JS to improve
+           functionality and interactivity.
+        </p>
         <ul>
-          <li>- Hellow</li>
-          <li>- bob</li>
-          <li>- yo</li>
+          <lh>Features</lh>
+          <li>- Browser size responsiveness</li>
+          <li>- Customer review system</li>
+          <li>- Interactive menu with drinks, toppings, and options</li>
+          <li>- Cart system to store user order</li>
+        </ul>
+      </Project>
+      <Project title="BlueAir Website"
+               src="./assets/BlueAir.png"
+               width="600px"
+               alt="Blue Website home page"
+               site="https://yu-xiao-blue-air.netlify.app/"
+               code="https://github.com/ytxpk/blue-air-website"
+               tech=" HTML, CSS, JS">
+        <p>A mock website meant for a business that delivers 
+           fresh air. Elegance and mobile usability was 
+           a focus for this project. Through this project, 
+           I enhanced my understanding of UI and UX 
+           principles in addition to JS tools such as
+           intersection observer.
+        </p>
+        <ul>
+          <lh>Features</lh>
+          <li>- Animated links</li>
+          <li>- Fade in home page image</li>
+          <li>- Mobile responsiveness with hamburger menu</li>
         </ul>
       </Project>
     </section>
