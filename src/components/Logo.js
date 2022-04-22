59b76e8c7e978bdd1e1ecc6a11a581da477bb7a0
ytxpk <txiaopk@gmail.com>
2022-04-21 21:36:51 -0400

    Improved Safari compatability

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,7 @@
 import { HashLink as Link } from 'react-router-hash-link';
 
 import './Button.css'
+import './Logo.css'
 
 export default function Logo() {
     return (
@@ -9,12 +10,14 @@ export default function Logo() {
           <div className="button-container">
             <img src="./assets/sig.png" 
                 alt="Yu Xiao signature" 
-                height="20px" 
+                height="20px"
+                width="40px" 
                 className="logo"/>
             <img src="./assets/sig.png" 
                 alt="Yu Xiao signature outline" 
                 height="20px" 
-                className="outline"/>
+                width="40px"
+                className="outline logo-outline"/>
           </div>
         </Link>
       </div>
