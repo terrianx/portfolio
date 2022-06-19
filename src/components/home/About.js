28265ec0bdc8b885be560bd836d1626aba787ebd
ytxpk <txiaopk@gmail.com>
2022-06-19 19:32:15 -0400

    Improved about section fade click through on intro section buttons

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -14,30 +14,32 @@ export default function About() {
   return (
     <section className="about">
       <div className="fade-box"></div>
+      <div className="cover-box"></div>
 
       <span className="anchor" id="about"></span>
 
-      <div className="about-container">
-        <span className="diag-line"></span>
+      <div className="about-skew-container">
+        <div className="about-container">
+          <span className="diag-line"></span>
 
-        <h1>About Me</h1>
+          <h1>About Me</h1>
 
-        <p>
-          I'm a computer science student studying
-          at the University of Michigan from the
-          Greater Boston area. My hope
-          is to make our world a safer and more
-          beautiful place to live in. I 
-          enjoy programming, illustrating,
-          board games, and hiking. Explore my 
-          projects and art at the links below.
-        </p>
+          <p>
+            I'm a computer science student studying
+            at the University of Michigan from the
+            Greater Boston area. My hope
+            is to make our world a safer and more
+            beautiful place to live in. I 
+            enjoy programming, illustrating,
+            board games, and hiking. Explore my 
+            projects and art at the links below.
+          </p>
 
-        <Button to="/projects#">Projects</Button>
-        <Button to="/gallery#">Gallery</Button>
+          <Button to="/projects#">Projects</Button>
+          <Button to="/gallery#">Gallery</Button>
 
+        </div>
       </div>
-
     </section>
   );
 }
